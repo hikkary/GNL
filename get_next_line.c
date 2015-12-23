69a75677474a8c030ee4aa25df9c52f00521d728
Zakaria KERKEB <zkerkeb@e3r12p5.42.fr>
2015-12-23 22:04:50 +0100

    que vois tu de la haut ?

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: zkerkeb <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/22 19:28:05 by zkerkeb           #+#    #+#             */
-/*   Updated: 2015/12/23 21:11:46 by zkerkeb          ###   ########.fr       */
+/*   Updated: 2015/12/23 22:04:24 by zkerkeb          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -26,7 +26,24 @@ int check_buff(char *buf)
 	return (0);
 }
 
-int len_line(int fd, char *buf, char **line)
+// fonction a modifier pour ecrire sur la ligne bn
+char *ft_write(int fd, char *buf, char **line , int bn)
+{
+	int i;
+	int len;
+	int ret;
+
+	len = 0;
+	i = 0;
+	while ((ret = read(fd, buf, BUFF_SIZE) != 0 && check_buff(buf) != 1))
+	{
+		len += ret;	
+		ft_putstr(buf);
+	}
+	return(len);
+}
+
+int len_line(int fd, char *buf, char **line , int bn)
 {
 	int i;
 	int len;
@@ -42,6 +59,8 @@ int len_line(int fd, char *buf, char **line)
 	return(len);
 }
 
+
+
 char *get_line(int const fd, char *buf, char **line)
 {
 	int i;
@@ -54,12 +73,14 @@ char *get_line(int const fd, char *buf, char **line)
 	return("ta race");
 }
 
+
+// integrer bn pour permettre la recursivite
 int get_next_line(int const fd, char **line)
 {
 	int i;
 	int fd2;	
 	char buf[BUFF_SIZE];
-	char *l;
+	static int bn;
 	int len;
 
 	fd2 = fd;
@@ -67,12 +88,11 @@ int get_next_line(int const fd, char **line)
 	fd2 = open(line[0], O_RDONLY);
 	//printf("%d", fd2);
 	//l = get_line(fd, buf, &argv[1]);
-	while (read(fd,buf, BUFF_SIZE))
-	{
-		len = len_line(fd2, buf, line); 
-		line[i] = (char *)malloc(sizeof(char) * len);
-	}
-	//printf(" %d", len);	
+	
+	len = len_line(fd2, buf, line, bn); 
+	line[i] = (char *)malloc(sizeof(char) * len);
+	line[i] = ft_write(fd2, buf, line, bn);
+	//printf(" %d", len);
 	return (0);
 }
 
