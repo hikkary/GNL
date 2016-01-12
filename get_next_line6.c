fb67335c38b5b7976263173fb4428566c476943e
hikkary <z.kerkeb@gmail.com>
2016-01-12 13:02:44 +0000

    ok

--- get_next_line6.c
+++ get_next_line6.c
@@ -12,61 +12,66 @@
 
 #include "get_next_line.h"
 #include <stdio.h>
-/*
-int check_buff(char *buf, int cara)
+
+int len_buff(char *buf)
 {
 	int i;
 
 	i = 0;
-	while (buf[i] != '\0')
+	while (buf[i])
 	{
-		if (buf[i] == cara)
+		if (buf[i] == '\n' || buf[i] == '\0' || buf[i]  == EOF)
 			return(i);
 		i++;
 	}
 	return (0);
 }
-*/
 
-t_g stock(t_g s, int fd)
+
+t_g *stock(t_g *s, int fd)
 {
 	int ret;
 
-	s.total = ft_strnew(BUFF_SIZE + 1);
-	while ((ret = read(fd, s.buf, BUFF_SIZE)))
+	s->total = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
+	while ((ret = read(fd, s->buf, BUFF_SIZE)))
 	{
-		s.buf[ret] = '\0';
-		s.total = ft_strjoin(s.total, s.buf);
+		s->buf[ret] = '\0';
+		s->total = ft_strjoin(s->total, s->buf);
 		if (fd == 0 && ret > 0)
 			return (s);
 	}
 	return (s);
-	
 }
 
-/*
-char **ft_write(char **line, char **b)
+
+t_g *ft_write(t_g s,char **line)
 {
 	int i;
 	
 	i = 0;
-	while (b[0][i] != '\n' && b[0][i] && b[0][i] != EOF)
+	while (s->total[i] != '\n' && s->total[i] && s->total[i] != EOF)
 	{
-		line[0][i] = b[0][i];
+		line[0][i] = s->total[0][i];
 		i++;
 	}
 	line[0][i] = '\0';
 	return (line);
 }
-*/
+
+// Le stdin semble fonctionner, faire en sorte que la sortie standard sois stocker dans lime[0]
 int get_next_line(int const fd, char **line)
 {
-	static t_g s;
-	ft_putstr("1");
+	static t_g *s;
+
+	if (!s)
+		s = malloc(sizeof(t_g));
 	s = stock(s,fd);
-	ft_putstr("2");
-	ft_putstr(s.total);
-	return (1);
+	line[0] = ft_strnew(len_buff(s->total));
+	s = ft_write(s, line[0]);
+//	ft_putstr(s->total);
+	
+
+	return (0);
 }
  
 int main(int argc, char **argv)
@@ -74,9 +79,9 @@ int main(int argc, char **argv)
 	char *line;
 	int fd;
 	fd = open(argv[1], O_RDONLY);
-	while(get_next_line(0, &line))
+	while(get_next_line(fd, &line))
 	{
-		ft_putendl(line);
+		//ft_putendl(line);
 		//free(line);
 	}
 	return (0);
