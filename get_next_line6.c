fb14511a256bc00d5a3054e6b9eef178c68a9196
Hikkary <z.kerkeb@gmail.com>
2016-01-12 19:31:14 +0100

    gnl tout le buff

--- get_next_line6.c
+++ get_next_line6.c
@@ -18,6 +18,8 @@ int len_buff(char *buf)
 	int i;
 
 	i = 0;
+	if (!buf)
+		return(1);
 	while (buf[i])
 	{
 		if (buf[i] == '\n' || buf[i] == '\0' || buf[i]  == EOF)
@@ -28,61 +30,76 @@ int len_buff(char *buf)
 }
 
 
-t_g *stock(t_g *s, int fd)
+t_g *ft_stock(t_g *s, int fd)
 {
 	int ret;
 
+	s->t = 0;
 	s->total = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
-	while ((ret = read(fd, s->buf, BUFF_SIZE)))
+	while ((ret = read(fd, s->buf, BUFF_SIZE)) )
 	{
 		s->buf[ret] = '\0';
 		s->total = ft_strjoin(s->total, s->buf);
 		if (fd == 0 && ret > 0)
 			return (s);
 	}
+	if (ret == 0)
+		s->t = 1;
 	return (s);
 }
 
 
-t_g *ft_write(t_g s,char **line)
+char *ft_write(t_g *s,char **line)
 {
 	int i;
 	
 	i = 0;
 	while (s->total[i] != '\n' && s->total[i] && s->total[i] != EOF)
 	{
-		line[0][i] = s->total[0][i];
+		line[0][i] = s->total[i];
 		i++;
 	}
 	line[0][i] = '\0';
-	return (line);
+	return (line[0]);
 }
 
-// Le stdin semble fonctionner, faire en sorte que la sortie standard sois stocker dans lime[0]
 int get_next_line(int const fd, char **line)
 {
 	static t_g *s;
-
+	if (fd < 0)
+		return (0);
 	if (!s)
 		s = malloc(sizeof(t_g));
-	s = stock(s,fd);
-	line[0] = ft_strnew(len_buff(s->total));
-	s = ft_write(s, line[0]);
-//	ft_putstr(s->total);
-	
-
-	return (0);
+	//ft_putnbr(s->t);
+	if (s->t != 1)
+		s = ft_stock(s,fd);
+	//DEBUG
+	line[0] = ft_strnew(len_buff(s->total) + 1);
+	//DEBUG
+	line[0] = ft_write(s, &line[0]);
+	//DEBUG
+	s->total = ft_strchr(s->total,'\n');
+	//DEBUG
+	s->total++;
+	//ft_putstr(s->total);
+	if((*s->total == '\0' || *s->total == EOF) && fd != 0)
+		s->stop = 2;
+	if(s->stop == 2)
+		return(0);
+	else
+		return (1);
 }
- 
+
 int main(int argc, char **argv)
 {
 	char *line;
 	int fd;
 	fd = open(argv[1], O_RDONLY);
+	//fd = 0;
 	while(get_next_line(fd, &line))
 	{
-		//ft_putendl(line);
-		//free(line);
+		ft_putendl(line);
+		free(line);
 	}
 	return (0);
 }
