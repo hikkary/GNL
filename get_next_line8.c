a2b4ade3144f52dac647f9c73339cbb13d4b3aab
Zakaria KERKEB <zkerkeb@e3r11p14.42.fr>
2016-01-14 16:10:44 +0100

    Hamdullah

--- get_next_line8.c
+++ get_next_line8.c
@@ -6,7 +6,7 @@
 /*   By: zkerkeb <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/13 22:15:07 by zkerkeb           #+#    #+#             */
-/*   Updated: 2016/01/13 23:21:38 by zkerkeb          ###   ########.fr       */
+/*   Updated: 2016/01/14 16:09:55 by zkerkeb          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,13 +36,31 @@ int get_next_line(int const fd, char **line)
 
 	if(!s)
 		s = malloc(sizeof(t_g));
-	line[0] = ft_strnew(BUFF_SIZE + 1);
+	
+	line[0] = ft_strnew(1);
+	if (s->an)
+	{
+		if(ft_strchr(s->an, '\n'))
+		{
+			line[0] = ft_strsub(s->an, 0, bn(s->an));
+			s->an  = ft_strchr(s->an, '\n');
+			s->an++;
+			if (*s->an == '\0')
+			{
+				s->an = NULL;
+				free(s->an);
+				return (1);
+			}
+			else
+				return (1);
+		}
+	}
 	if(!s->an) 
 	{
 		s->an = ft_strnew(BUFF_SIZE + 1);
 		s->an = NULL;
 	}
-	if ((ret = read(fd, s->buf,BUFF_SIZE)) && !s->an) // is si il y a quelque chose a lire
+	if ((ret = read(fd, s->buf,BUFF_SIZE))) // is si il y a quelque chose a lire
 	{
 		s->buf[ret] = '\0';
 		line[0] = ft_strjoin(line[0], s->buf);
@@ -52,12 +70,12 @@ int get_next_line(int const fd, char **line)
 	{
 		s->an++;
 		line[0] = ft_strsub(line[0], 0, bn(line[0]));	
-	}	
-	
-	if(line[0])	
-		return (1);
-	else
+	}
+//	printf("%s", line[0]);
+	if(line[0][0] == '\0' && ret == 0)
 		return (0);
+	else
+		return (1);
 }
 
 int main(int argc, char **argv)
@@ -65,8 +83,8 @@ int main(int argc, char **argv)
 	char *line;
 	int fd;
 
-	fd = open(argv[1], O_RDONLY);
-	
+	//fd = open(argv[1], O_RDONLY);
+	fd = 0;
 	while(get_next_line(fd, &line))
 	{
 		ft_putendl(line);
