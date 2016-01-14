e38dc62be85c519618ad47882553da50d61c9590
Zakaria KERKEB <zkerkeb@e3r11p14.42.fr>
2016-01-14 23:53:28 +0100

    VIVE LA FRANCE

--- get_next_line8.c
+++ get_next_line8.c
@@ -6,7 +6,7 @@
 /*   By: zkerkeb <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/13 22:15:07 by zkerkeb           #+#    #+#             */
-/*   Updated: 2016/01/14 17:58:26 by zkerkeb          ###   ########.fr       */
+/*   Updated: 2016/01/14 23:52:37 by zkerkeb          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,40 +21,45 @@ int bn(char *buf)
 
 	while (buf[i])
 	{
-		if(buf[i] == '\n' || buf[i] == EOF)
+		if(buf[i] == '\n' ||  buf[i] == EOF)
 			return(i);
 		i++;
 	}
 	return (0);
 }
 
-char *rline(t_g *s,char **line, int fd)
+char *lire(t_g *s, int fd, char **line)
 {
 
-	if ((s->ret = read(fd, s->buf,BUFF_SIZE))) // is si il y a quelque chose a lire
+	while ((s->ret = read(fd, s->buf,BUFF_SIZE))) // is si il y a quelque chose a lire
 	{
-		s->buf[s->ret] = '\0';	
+		if(s->an != NULL)
+			line[0] = ft_strjoin(line[0],s->an);
+		s->buf[s->ret] = '\0';
 		line[0] = ft_strjoin(line[0], s->buf);
-		if (ft_strchr(line[0],'\n') == NULL)// || ft_strchr(line[0], EOF) == NULL)
-			rline(s, &line[0], fd);
-		if ((s->an = ft_strchr(line[0],'\n'))) //stock le pointeur sur \n dans s->an
+		if(ft_strchr(line[0], '\n'))
 		{
+//			printf("\nan 1 = %s\n ", s->an);
+			s->an = ft_strchr(line[0],'\n');
 			s->an++;
 			line[0] = ft_strsub(line[0], 0, bn(line[0]));
+			return(line[0]);
 		}
 	}
-	return (line[0]);
+	return(line[0]);
 }
 
-
-// faire fonction qui va gerer after \n
+//	faire fonction qui va gerer after \n
 int get_next_line(int const fd, char **line)
 {
 	static t_g *s;
 	int ret;
 
 	if(!s)
+	{
 		s = malloc(sizeof(t_g));
+		s->an = NULL;
+	}
 	line[0] = ft_strnew(1);
 	if (s->an)
 	{
@@ -73,14 +78,16 @@ int get_next_line(int const fd, char **line)
 				return (1);
 		}
 	}
-	if(!s->an) 
-	{
-		s->an = ft_strnew(BUFF_SIZE + 1);
-		s->an = NULL;
-	}
-	line[0] = rline(s, &line[0], fd);
-//	printf("%s", line[0]);
-	if(line[0][0] == '\0' && ret == 0)
+
+	//iif(!s->an) 
+//	{
+	//	s->an = ft_strnew(BUFF_SIZE + 1);
+//	}
+
+	line[0] = lire(s, fd, &line[0]);
+	//	printf("%s", line[0]);
+	//printf("\nan 2 = %s\n ", s->an);
+	if(line[0][0] == '\0' && s->ret == 0)
 		return (0);
 	else
 		return (1);
@@ -91,8 +98,8 @@ int main(int argc, char **argv)
 	char *line;
 	int fd;
 
-	fd = open(argv[1], O_RDONLY);
-	//fd = 0;
+//	fd = open(argv[1], O_RDONLY);
+	fd = 0;
 	while(get_next_line(fd, &line))
 	{
 		ft_putendl(line);
