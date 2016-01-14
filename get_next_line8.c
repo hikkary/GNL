fe8cc0a1784fc8c57711765835ef7add6e9a8690
Zakaria KERKEB <zkerkeb@e3r11p14.42.fr>
2016-01-14 17:58:53 +0100

    regler le pb des petit buffer qui coupe

--- get_next_line8.c
+++ get_next_line8.c
@@ -6,7 +6,7 @@
 /*   By: zkerkeb <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/13 22:15:07 by zkerkeb           #+#    #+#             */
-/*   Updated: 2016/01/14 16:09:55 by zkerkeb          ###   ########.fr       */
+/*   Updated: 2016/01/14 17:58:26 by zkerkeb          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,31 @@ int bn(char *buf)
 
 	while (buf[i])
 	{
-		if(buf[i] == '\n')
+		if(buf[i] == '\n' || buf[i] == EOF)
 			return(i);
 		i++;
 	}
 	return (0);
 }
+
+char *rline(t_g *s,char **line, int fd)
+{
+
+	if ((s->ret = read(fd, s->buf,BUFF_SIZE))) // is si il y a quelque chose a lire
+	{
+		s->buf[s->ret] = '\0';	
+		line[0] = ft_strjoin(line[0], s->buf);
+		if (ft_strchr(line[0],'\n') == NULL)// || ft_strchr(line[0], EOF) == NULL)
+			rline(s, &line[0], fd);
+		if ((s->an = ft_strchr(line[0],'\n'))) //stock le pointeur sur \n dans s->an
+		{
+			s->an++;
+			line[0] = ft_strsub(line[0], 0, bn(line[0]));
+		}
+	}
+	return (line[0]);
+}
+
 
 // faire fonction qui va gerer after \n
 int get_next_line(int const fd, char **line)
@@ -36,7 +55,6 @@ int get_next_line(int const fd, char **line)
 
 	if(!s)
 		s = malloc(sizeof(t_g));
-	
 	line[0] = ft_strnew(1);
 	if (s->an)
 	{
@@ -60,17 +78,7 @@ int get_next_line(int const fd, char **line)
 		s->an = ft_strnew(BUFF_SIZE + 1);
 		s->an = NULL;
 	}
-	if ((ret = read(fd, s->buf,BUFF_SIZE))) // is si il y a quelque chose a lire
-	{
-		s->buf[ret] = '\0';
-		line[0] = ft_strjoin(line[0], s->buf);
-	}
-
-	if ((s->an = ft_strchr(line[0],'\n'))) //stock le pointeur sur \n dans s->an
-	{
-		s->an++;
-		line[0] = ft_strsub(line[0], 0, bn(line[0]));	
-	}
+	line[0] = rline(s, &line[0], fd);
 //	printf("%s", line[0]);
 	if(line[0][0] == '\0' && ret == 0)
 		return (0);
@@ -83,8 +91,8 @@ int main(int argc, char **argv)
 	char *line;
 	int fd;
 
-	//fd = open(argv[1], O_RDONLY);
-	fd = 0;
+	fd = open(argv[1], O_RDONLY);
+	//fd = 0;
 	while(get_next_line(fd, &line))
 	{
 		ft_putendl(line);
