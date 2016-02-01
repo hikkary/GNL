a08a024d0b6d8834d3acc3a9638a1ef1b507af23
Zakaria KERKEB <zkerkeb@e3r6p7.42.fr>
2016-02-01 14:32:53 +0100

    ok

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: zkerkeb <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/30 17:30:47 by zkerkeb           #+#    #+#             */
-/*   Updated: 2016/01/30 18:17:37 by zkerkeb          ###   ########.fr       */
+/*   Updated: 2016/01/30 18:46:00 by zkerkeb          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 100
-# include "libft/includes/libft.h"
+# define BUFF_SIZE 1
+# include "libft.h"
 # include <unistd.h>
 # include <stdlib.h>
 # include <sys/types.h>
